73e3a417154fbc887a90a4e8938869585fa555e3
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-03 16:45:02 -0300

    feat(product cards): Imagenes y precio formateado

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,27 +1,28 @@
 import { ProductCard } from "@/components/product-card";
+import manzanas from "@/assets/manzanas.png";
+import cocacola from "@/assets/cocacola.png";
 
 export default function Home() {
   const recommendations = [
-    { id: 1, name: "Producto 1", price: 3 },
-    { id: 2, name: "Producto 2", price: 4 },
-    { id: 3, name: "Producto 3", price: 5 },
-    { id: 4, name: "Producto 4", price: 6 },
+    { id: 1, name: "50x Manzanas", price: 40, image: manzanas },
+    { id: 2, name: "50x Coca Cola", price: 50, image: cocacola },
+    { id: 3, name: "100x Manzanas", price: 70, image: manzanas },
+    { id: 4, name: "100x Coca Cola", price: 80, image: cocacola },
   ];
 
   return (
-    <main className="bg-background">
-      <div className="container px-4 py-8 mx-auto">
-        <h2 className="mb-6 text-xl font-medium">Nuestras recomendaciones</h2>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-          {recommendations.map((product) => (
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              price={product.price}
-            />
-          ))}
-        </div>
+    <>
+      <h2 className="mb-6 text-xl font-medium">Nuestras recomendaciones</h2>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        {recommendations.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+          />
+        ))}
       </div>
-    </main>
+    </>
   );
 }
--- client/src/components/product-card.tsx
+++ client/src/components/product-card.tsx
@@ -1,27 +1,35 @@
-import Link from "next/link";
-import { Card, CardContent } from "@/components/ui/card";
+// import Link from "next/link";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image, { type StaticImageData } from "next/image";
+import { formatPrice } from "@/lib/utils";
 
 interface ProductCardProps {
   name: string;
   price: number;
+  image: StaticImageData;
 }
 
-export function ProductCard({ name, price }: ProductCardProps) {
+export function ProductCard({ name, price, image }: ProductCardProps) {
   return (
-    <Link href="#" className="block transition-opacity hover:opacity-80">
-      <Card className="overflow-hidden">
-        <div className="aspect-square">
-          <div className="flex items-center justify-center w-full h-full">
-            Imagen
-          </div>
+    // <Link href="#" className="block transition-opacity hover:opacity-80">
+    <Card className="overflow-hidden rounded-none pt-0">
+      <CardHeader className="p-0">
+        <Image
+          src={image}
+          placeholder="blur"
+          alt="Product Image"
+          width={300}
+          height={300}
+          className="object-cover w-full h-full"
+        />
+      </CardHeader>
+      <CardContent className="">
+        <div className="flex items-center justify-between">
+          <p className="text-sm font-medium truncate">{name}</p>
+          <p className="text-sm font-medium">${formatPrice(price)}</p>
         </div>
-        <CardContent className="p-3">
-          <div className="flex items-center justify-between">
-            <p className="text-sm font-medium truncate">{name}</p>
-            <p className="text-sm font-medium">${price}</p>
-          </div>
-        </CardContent>
-      </Card>
-    </Link>
+      </CardContent>
+    </Card>
+    // </Link>
   );
 }

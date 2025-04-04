1cd193c30f75f73460271471f23643058082d7f0
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-04 18:58:03 -0300

    feat(productos): mover recomendaciones a un nuevo archivo de datos

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,7 @@
 import { ProductCard } from "@/components/product-card";
-import manzanas from "@/assets/manzanas.png";
-import cocacola from "@/assets/cocacola.png";
+import { recommendations } from "@/data/products";
 
 export default function Home() {
-  const recommendations = [
-    { id: 1, name: "50x Manzanas", price: 40, image: manzanas },
-    { id: 2, name: "50x Coca Cola", price: 50, image: cocacola },
-    { id: 3, name: "100x Manzanas", price: 70, image: manzanas },
-    { id: 4, name: "100x Coca Cola", price: 80, image: cocacola },
-  ];
-
   return (
     <>
       <h2 className="mb-6 text-xl font-medium">Nuestras recomendaciones</h2>
---
+++ client/src/data/products.ts
@@ -0,0 +1,9 @@
+import manzanas from "@/assets/manzanas.png";
+import cocacola from "@/assets/cocacola.png";
+
+export const recommendations = [
+  { id: 1, name: "50x Manzanas", price: 40, image: manzanas },
+  { id: 2, name: "50x Coca Cola", price: 50, image: cocacola },
+  { id: 3, name: "100x Manzanas", price: 70, image: manzanas },
+  { id: 4, name: "100x Coca Cola", price: 80, image: cocacola },
+];

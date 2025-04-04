17b31e60962a218d7b7c0109089abd2257d279f5
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-04 19:12:25 -0300

    fix(logo): otro arreglin temporal en la carga del logo

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import logoDark from "@/assets/logo-dark.svg";
-import logoWhite from "@/assets/logo-white.svg";
-import { useTheme } from "next-themes";
+// import logoDark from "@/assets/logo-dark.svg";
+// import logoWhite from "@/assets/logo-white.svg";
+import logoNormal from "@/assets/logo.png";
+// import { useTheme } from "next-themes";
 
 export const Logo = () => {
-  const { theme } = useTheme();
-  const logosvg = theme === "dark" ? logoWhite : logoDark;
-  return <Image src={logosvg} alt="Logo de Myno" width={32} height={32} />;
+  // const { theme } = useTheme();
+  // const logosvg = theme === "dark" ? logoWhite : logoDark;
+  return <Image src={logoNormal} alt="Logo de Myno" width={32} height={32} />;
 };

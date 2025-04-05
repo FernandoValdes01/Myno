948a98f24b31f35087a1115799c2613df78753ce
Vicente Rivera <vrivera.dev@gmail.com>
2025-04-05 17:41:50 -0300

    feat(productos): Página dedicada para cada producto

---
+++ client/src/app/products/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import ProductDetail from "@/components/product-details";
+import { recommendations } from "@/data/products";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+
+  const product = recommendations.find((product) => product.slug === slug);
+  if (!product) notFound();
+
+  return <ProductDetail product={product} relatedProducts={recommendations} />;
+}

e3ed3597a5b3b8d71e12148e11f736b61bbf4818
Juanfrax2 <juan.munoz.veloso@gmail.com>
2025-04-10 00:42:38 -0400

    chore(db): Migración inicial para la base de datos

--- Server/core/settings.py
+++ Server/core/settings.py
@@ -72,15 +72,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'myno',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- Server/mi_app/models.py
+++ Server/mi_app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=255)
+    correo = models.EmailField(max_length=100)
+    apellido = models.CharField(max_length=255)
+    empresa = models.CharField(max_length=255)
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    image = models.CharField(max_length=255)  
+    slug = models.SlugField(unique=True)  
+
+    def __str__(self):
+        return self.nombre
+
+class Precio(models.Model):
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+class Stock(models.Model):
+    cantidad = models.IntegerField()
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+class Venta(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    total_sin_IVA = models.DecimalField(max_digits=10, decimal_places=2)
+    total_con_IVA = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_venta = models.DateField()
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+    class Meta:
+        unique_together = ('venta', 'producto')
+
+
